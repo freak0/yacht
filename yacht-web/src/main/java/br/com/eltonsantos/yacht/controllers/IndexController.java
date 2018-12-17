5758553c73dee5099b3f8a4df6b173efe11bbd9c
Elton Santos <eltonads@gmail.com>
2018-12-17 15:11:24 -0200

    Added Thymeleaf dep and the basic index page and controller. Closes #4

---
+++ yacht-web/src/main/java/br/com/eltonsantos/yacht/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package br.com.eltonsantos.yacht.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getIndexPage(Model model) {
+        return "main";
+    }
+}
