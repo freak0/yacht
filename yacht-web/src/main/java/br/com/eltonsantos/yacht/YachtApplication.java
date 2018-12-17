79c86f4656dec72bb34b9a1a5205dca5e0952450
Elton Santos <eltonads@toptal.com>
2018-12-17 10:09:47 -0200

    Initial Commit

---
+++ yacht-web/src/main/java/br/com/eltonsantos/yacht/YachtApplication.java
@@ -0,0 +1,14 @@
+package br.com.eltonsantos.yacht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YachtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YachtApplication.class, args);
+    }
+
+}
+
